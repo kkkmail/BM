fd6431500c3380f011899f98d2cfb8fd836b710d
kkkmail <kkk@jfcglobal.net>
2018-08-25 13:12:55 -0400

    20180825.01

--- Berreman/Berreman/Tests.fsx
+++ Berreman/Berreman/Tests.fsx
@@ -3,4 +3,11 @@
 #r "System.Core.dll"
 #r "System.dll"
 #r "System.Numerics.dll"
+//===========================================================
+#r "../packages/MathNet.Numerics.4.5.1/lib/net461/MathNet.Numerics.dll"
+#r "../packages/MathNet.Numerics.FSharp.4.5.1/lib/net45/MathNet.Numerics.FSharp.dll"
+#r "../packages/System.ValueTuple.4.5.0/lib/net47/System.ValueTuple.dll"
+//===========================================================
+#r "./bin/Debug/Berreman.exe"
+//===========================================================
 
