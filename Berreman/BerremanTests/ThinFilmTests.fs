54d03dd7011c5a0d4fdc4596ec3e6c148cdbf2f1
kkkmail <kkk@jfcglobal.net>
2018-08-31 03:10:08 -0400

    20180831.01

---
+++ Berreman/BerremanTests/ThinFilmTests.fs
@@ -0,0 +1,31 @@
+﻿namespace BerremanTests
+
+open Berreman.MathNetNumericsMath
+open Berreman.Geometry
+open Berreman.Fields
+open Berreman.MaterialProperties
+open Berreman.BerremanMatrix
+
+open Xunit
+open Xunit.Abstractions
+
+open MatrixComparison
+
+type ThinFilmTestData =
+    {
+        opticalProperties : OpticalProperties
+        n1SinFita : N1SinFita
+        expected : ComplexMatrix
+    }
+
+type ThinFilmTests(output : ITestOutputHelper) =
+
+    let data = 
+        [
+        ]
+
+    member __.runTest (d : ThinFilmTestData) = failwith ""
+
+    [<Fact>]
+    member this.berremanMatrixTest0 () = this.runTest (data.[0])
+
